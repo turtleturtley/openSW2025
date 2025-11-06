7ee8657488fe8e81928f6abb78378e1493da6ee8
tutletutley <mg7694@naver.com>
2025-11-06 15:00:23 +0900

    new foofoo and foofoofoo are defined

--- git1106/src/foo.c
+++ git1106/src/foo.c
@@ -1,5 +1,13 @@
 #include "func1.h"
 
 void foo(void)  {
-    printf("Hello foo!");
+    printf("Hello foo!\n");
+}
+
+void foofoo(void)  {
+    printf("Hello foofoo!\n");
+}
+
+void foofoofoo(void)  {
+    printf("Hello foofoofoo!\n");
 }
